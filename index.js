bc4543e5d10b7ceb4f898d7c507b1c34c6d43549
Jason Lampel <jason@abettermap.com>
2023-08-10 14:12:30 -0500

    simple-import-sort: restore React rule

--- index.js
+++ index.js
@@ -291,6 +291,8 @@ module.exports = {
           // `^(${require("module").builtinModules.join("|")})(/|$)`
           // eslint-disable-next-line prettier/prettier
           ['^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'],
+          // Packages. `react` related packages come first. Leave in so Jason can use 💚
+          ['^react', '^@?\\w'],
           // Internal packages.
           ['^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)'],
           // Side effect imports.
